4405f6ecf253a69335224e88938b21f0c7b4c117
Nahima ABBASSI <nabbassi@e1r1p12.42.fr>
2020-09-17 14:42:19 +0200

    2eme commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,85 @@
 /*   By: nabbassi <nabbassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 16:02:28 by nabbassi          #+#    #+#             */
-/*   Updated: 2020/08/20 14:22:12 by nabbassi         ###   ########.fr       */
+/*   Updated: 2020/09/17 14:38:44 by nabbassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+**	This function allow to find the position of the index
+**	untill the end of the line.
+*/
+
+static int		ft_endofline(const char *s)
+{
+	int				i;
+	int				end_of_line_found;
+
+	i = 0;
+	end_of_line_found = 0;
+	while (s[i] != '\0' && !end_of_line_found)
+	{
+		if (s[i] == '\n')
+			end_of_line_found = 1;
+		else
+			i++;
+	}
+	return (i);
+}
+
+/*
+**	This function is used to read each line untill the end of the file.
+*/
+
+static char		*ft_lineread(char *stock)
+{
+	char		*str;
+
+	str = ft_strchr(stock, '\n');
+	if (str || (ft_endofline(stock) > 0))
+	{
+		if (str)
+			ft_strcpy(stock, str + 1);
+		else
+			stock[0] = '\0';
+		return (stock);
+	}
+	return (NULL);
+}
+
+/*
+**	The GNL function, first, checks for basic errors.
+**
+**	While there is no line (\n terminated) available in the static buffer,
+**	and there is some content to read in the file.
+**  It adds it at the end of the static buffer.
+**
+**  Then it extracts the line to return (if there is something to extract).
+**	If returns '0' if there were no content.
+*/
+
+int				get_next_line(int const fd, char **line)
+{
+	char		buff[BUFF_SIZE + 1];
+	static char	*stock;
+	int			res;
+	char		*ptr;
+
+	if (fd < 0 || BUFF_SIZE < 1 || !line || read(fd, buff, 0) < 0)
+		return (-1);
+	if (!stock && (stock = ft_strnew(0)) == NULL)
+		return (-1);
+	while (!(ft_strchr(stock, '\n')) && (res = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[res] = '\0';
+		ptr = stock;
+		stock = ft_strjoin(ptr, buff);
+		ft_strdel(&ptr);
+	}
+	*line = ft_strsub(stock, 0, ft_endofline(stock));
+	if (ft_lineread(stock) == NULL)
+		return (0);
+	return (1);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,23 @@
 /*   By: nabbassi <nabbassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 14:22:57 by nabbassi          #+#    #+#             */
-/*   Updated: 2020/08/20 14:22:58 by nabbassi         ###   ########.fr       */
+/*   Updated: 2020/09/17 14:34:47 by nabbassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1024
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+# include <fcntl.h>
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
